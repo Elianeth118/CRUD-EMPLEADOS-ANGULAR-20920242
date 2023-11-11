48b5adaed7600434d90e1c4949be2010472df9cd
Elianeth118 <aragonelianeth@gmail.com>
2023-11-11 00:32:25 -0600

    CRUD-EMPLEADOS-ANGULAR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
+import { CreateEmpleadoComponent } from './components/create-empleado/create-empleado.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { EmpleadoComponent } from './components/empleado/empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmpleadosComponent,
+    CreateEmpleadoComponent,
+    NavBarComponent,
+    EmpleadoComponent
   ],
   imports: [
     BrowserModule,
